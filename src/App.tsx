724aac6d2472c35577ec4372f8e2c35e970119ec
e-roy <ericroupe@gmail.com>
2022-06-01 11:19:32 -0400

    test prod

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,19 @@ import { LensLogin } from "./components/wallet";
 
 import { LandingPage, ProfilePage } from "./pages";
 
-import { ENV_PROD, ENV_DEV, IS_PRODUCTION } from "@/constants";
+import {
+  ENV_PROD,
+  ENV_DEV,
+  IS_PRODUCTION,
+  CURRENT_CHAIN_ID,
+  CURRENT_CHAIN_NAME,
+} from "@/constants";
 
 console.log("is production", IS_PRODUCTION);
 console.log("ENV_PROD", ENV_PROD);
 console.log("ENV_DEV", ENV_DEV);
+console.log("CURRENT_CHAIN_ID", CURRENT_CHAIN_ID);
+console.log("CURRENT_CHAIN_NAME", CURRENT_CHAIN_NAME);
 
 function App() {
   return (
